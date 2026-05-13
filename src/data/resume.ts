a09353cb44e2d024bb61e220fbaf018d058bd224
russlib <email@example.com>
2026-05-13 16:12:19 -0700

    update resume URL to S3b PDF

--- src/data/resume.ts
+++ src/data/resume.ts
@@ -1,2 +1,2 @@
 export const resumeUrl =
-  "https://docs.google.com/document/d/10wseeT6F8uoczMt6xi6d9QlnCHSX6pKJ/preview";
+  "https://drive.google.com/file/d/1B1U9kDBu32IF2iEJiai4eluiAg1kiPgT/view";
